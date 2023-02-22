cf7cc09ef9d670918986ae36246d98cdff4af785
Manoj Hiray <manojhiray8@gmail.com>
2023-02-22 17:32:12 +0530

    AzureCRUDProject

---
+++ AzureTableStorageCRUDoperation/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureTableStorageCRUDoperation
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string id)
+        {
+            // for edit view
+            if (!string.IsNullOrEmpty(id))
+            {
+                // Set the name of the table
+                TableManager TableManagerObj = new TableManager("person");
+
+                // Retrieve the student object where RowKey eq value of id
+                List<Student> SutdentListObj = TableManagerObj.RetrieveEntity<Student>("RowKey eq '" + id + "'");
+
+                Student StudentObj = SutdentListObj.FirstOrDefault();
+                return View(StudentObj);
+            }
+
+            // new entry view
+            return View(new Student());
+        }
+
+        [HttpPost]
+        public ActionResult Index(string id, FormCollection formData)
+        {
+            Student StudentObj = new Student();
+            StudentObj.Name = formData["Name"] == "" ? null : formData["Name"];
+            StudentObj.Department = formData["Department"] == "" ? null : formData["Department"];
+            StudentObj.Email = formData["Email"] == "" ? null : formData["Email"];
+
+            // Insert
+            if (string.IsNullOrEmpty(id))
+            {
+                StudentObj.PartitionKey = "Student";
+                StudentObj.RowKey = Guid.NewGuid().ToString();
+
+                TableManager TableManagerObj = new TableManager("person");
+                TableManagerObj.InsertEntity<Student>(StudentObj, true);
+            }
+            // Update
+            else
+            {
+                StudentObj.PartitionKey = "Student";
+                StudentObj.RowKey = id;
+
+                TableManager TableManagerObj = new TableManager("person");
+                TableManagerObj.InsertEntity<Student>(StudentObj, false);
+            }
+
+            return RedirectToAction("Get");
+        }
+
+        public ActionResult Get()
+        {
+            TableManager TableManagerObj = new TableManager("person");
+            // Get all Student object, pass null as query
+            List<Student> SutdentListObj = TableManagerObj.RetrieveEntity<Student>(null);
+            return View(SutdentListObj);
+        }
+
+        public ActionResult ChangeStatus(string id, bool Status)
+        {
+            TableManager TableManagerObj = new TableManager("person");
+            List<Student> SutdentListObj = TableManagerObj.RetrieveEntity<Student>("RowKey eq '" + id + "'");
+            Student StudentObj = SutdentListObj.FirstOrDefault();
+            StudentObj.IsActive = !Status;
+            TableManagerObj.InsertEntity<Student>(StudentObj, false);
+
+            return RedirectToAction("Get");
+        }
+
+        public ActionResult Delete(string id)
+        {
+            // Retrieve the object to Delete
+            TableManager TableManagerObj = new TableManager("person");
+            List<Student> SutdentListObj = TableManagerObj.RetrieveEntity<Student>("RowKey eq '" + id + "'");
+            Student StudentObj = SutdentListObj.FirstOrDefault();
+
+            // Delete the object
+            TableManagerObj.DeleteEntity<Student>(StudentObj);
+            return RedirectToAction("Get");
+        }
+    }
+}
---
+++ AzureTableStorageCRUDoperation/TableHandling/TableManager.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+
+namespace AzureTableStorageCRUDoperation
+{
+    public class TableManager
+    {
+        /// <summary>
+        /// Cloud Table object
+        /// </summary>
+        private CloudTable table;
+
+        /// <summary>
+        /// Create Cloud Table object
+        /// </summary>
+        /// <param name="TableName">Name of the Table Name</param>
+        public TableManager(string TableName)
+        {
+            // Check if Table Name is blank
+            if (string.IsNullOrEmpty(TableName))
+            {
+                throw new ArgumentNullException("Table", "Table Name can't be empty");
+            }
+            try
+            {
+                // Get azure table storage connection string.
+                string ConnectionString = "https://Pro-Services-Design@dev.azure.com/Pro-Services-Design/Pro%20Services%20Design/_git/Universal%20Reporting%20System";
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConnectionString);
+
+                // Create the table if not exist and put the refarence of the table into Cloud Table object
+                CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+                table = tableClient.GetTableReference(TableName);
+                table.CreateIfNotExists();
+            }
+            catch (Exception ExceptionObj)
+            {
+                throw ExceptionObj;
+            }
+        }
+
+        /// <summary>
+        /// Insert or Update Method
+        /// </summary>
+        /// <typeparam name="T">Entity Type</typeparam>
+        /// <param name="entity">T type object</param>
+        /// <param name="forInsert">true for Insert, false for Update</param>
+        public void InsertEntity<T>(T entity, bool forInsert = true) where T : TableEntity, new()
+        {
+            try
+            {
+                if (forInsert)
+                {
+                    var insertOperation = TableOperation.Insert(entity);
+                    table.Execute(insertOperation);
+                }
+                else
+                {
+                    var insertOrMergeOperation = TableOperation.InsertOrReplace(entity);
+                    table.Execute(insertOrMergeOperation);
+                }
+            }
+            catch (Exception ExceptionObj)
+            {
+                throw ExceptionObj;
+            }
+        }
+
+        /// <summary>
+        /// Retrieve List of T type entity
+        /// </summary>
+        /// <typeparam name="T">Returned Entity Type</typeparam>
+        /// <param name="Query"></param>
+        /// <returns>List of T type object</returns>
+        public List<T> RetrieveEntity<T>(string Query = null) where T : TableEntity, new()
+        {
+            try
+            {
+                TableQuery<T> DataTableQuery = new TableQuery<T>();
+                if (!string.IsNullOrEmpty(Query))
+                {
+                    DataTableQuery = new TableQuery<T>().Where(Query);
+                }
+                IEnumerable<T> IDataList = table.ExecuteQuery(DataTableQuery);
+                List<T> DataList = new List<T>();
+                foreach (var singleData in IDataList)
+                    DataList.Add(singleData);
+                return DataList;
+            }
+            catch (Exception ExceptionObj)
+            {
+                throw ExceptionObj;
+            }
+        }
+
+        /// <summary>
+        /// Delete the entity
+        /// </summary>
+        /// <typeparam name="T">Entity Type</typeparam>
+        /// <param name="entity">T type entity</param>
+        /// <returns>true if able to delete</returns>
+        public bool DeleteEntity<T>(T entity) where T : TableEntity, new()
+        {
+            try
+            {
+                var DeleteOperation = TableOperation.Delete(entity);
+                table.Execute(DeleteOperation);
+                return true;
+            }
+            catch (Exception ExceptionObj)
+            {
+                throw ExceptionObj;
+            }
+        }
+    }
+}
