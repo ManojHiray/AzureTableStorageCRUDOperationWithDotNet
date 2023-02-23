d68fcb8e652a1538d11b7920b5236d60f3092367
Manoj Hiray <manojhiray8@gmail.com>
2023-02-23 15:53:46 +0530

    First Commit

--- AzureTableStorageCRUDoperation/App_Start/RouteConfig.cs
+++ AzureTableStorageCRUDoperation/App_Start/RouteConfig.cs
@@ -16,6 +16,7 @@ namespace AzureTableStorageCRUDoperation
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
+
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
